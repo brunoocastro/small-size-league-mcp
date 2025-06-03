b3cb6dc7a58ea8ca8b25475686155c1957163695
Bruno Castro (Tone) <bruno.castro@capyba.com>
2025-06-03 09:39:35 -0300

    feat: add LLM_API_KEY validation and update settings for OpenAI provider

--- providers.py
+++ providers.py
@@ -5,19 +5,26 @@ settings = MCP_Settings()
 if settings.LLM_PROVIDER == LLM_Providers.OPENAI:
     from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
+    if not settings.LLM_API_KEY:
+        raise ValueError(
+            "LLM_API_KEY must be set when using OpenAI as the LLM provider."
+        )
+
     llm_chat_provider = ChatOpenAI(
         model=settings.LLM_MODEL,
         api_key=settings.LLM_API_KEY,
-        base_url=settings.LLM_HOST,
+        base_url=settings.LLM_BASE_URL,
     )
 
     embedding_provider = OpenAIEmbeddings(
-        model=settings.EMBEDDING_MODEL, api_key=settings, base_url=settings.LLM_HOST
+        model=settings.EMBEDDING_MODEL, api_key=settings, base_url=settings.LLM_BASE_URL
     )
 else:
     from langchain_ollama import ChatOllama, OllamaEmbeddings
 
-    llm_chat_provider = ChatOllama(model=settings.LLM_MODEL, host=settings.LLM_HOST)
+    llm_chat_provider = ChatOllama(
+        model=settings.LLM_MODEL, base_url=settings.LLM_BASE_URL
+    )
     embedding_provider = OllamaEmbeddings(
-        model=settings.EMBEDDING_MODEL, host=settings.LLM_HOST
+        model=settings.EMBEDDING_MODEL, base_url=settings.LLM_BASE_URL
     )
--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 from enum import Enum
+from typing import Optional
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -14,10 +16,16 @@ class MCP_Settings(BaseSettings):
     This class uses Pydantic to manage configuration settings.
     """
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
     LLM_PROVIDER: LLM_Providers = LLM_Providers.OPENAI
-    LLM_API_KEY: str = ""
-    LLM_HOST: str = "https://api.openai.com/v1/chat/completions"
+    # If provider is OPENAI, the API KEY is required.
+    LLM_API_KEY: Optional[str] = Field(
+        default=None,
+        description="API key for the LLM provider. Required if provider is OPENAI.",
+    )
     LLM_MODEL: str = "gpt-4o-mini"
     EMBEDDING_MODEL: str = "text-embedding-3-small"
+    LLM_BASE_URL: Optional[str] = None
